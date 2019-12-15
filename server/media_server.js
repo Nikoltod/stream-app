6efca8b70fa8934948928ff9bba5b2be25020215
Todor Nikolov <todor_nikolov@ymail.com>
2019-12-15 20:46:31 +0200

    Adding the User to the media_server.js

--- server/media_server.js
+++ server/media_server.js
@@ -1,13 +1,25 @@
 const NodeMediaServer = require('node-media-server')
 const config = require('./config/default').rtmp_server
+const User = require('./database/Schema').User
 
 const nms = new NodeMediaServer(config)
 
 nms.on('prePublish', async (id, StreamPath, args) => {
     let stream_key = getStreamKeyFromStreamPath(StreamPath)
     console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`)
+ 
+    User.findOne({stream_key: stream_key}, (err, user) => {
+        if (!err) {
+            if (!user) {
+                let session = nms.getSession(id)
+                session.reject();
+            } else {
+                // do cool stuff
+            }
+        }
+    })
 })
-
+ 
 const getStreamKeyFromStreamPath = (path) => {
     let parts = path.split('/')
     return parts[parts.length - 1]
